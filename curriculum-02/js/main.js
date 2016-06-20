8662f42a08d0ed4d257a0f8062dee9a3fdfe3c32
hiroki-ueno <tmge_0618@i.softbank.jp>
2016-06-20 12:23:05 +0900

    カリキュラム２のjQueryを修正。

--- curriculum-02/js/main.js
+++ curriculum-02/js/main.js
@@ -1,11 +1,4 @@
 $(function(){
-//
-//01.main_wrap_top:hover
-//02.sns_link:click
-//03.footer_link_box:hover
-//
-//01.main_wrap_top:hover
-//
 $('.dgfruit_nav ul > li').hover(
     function(){
         $(this).find('a > img').stop(true).animate({opacity:0},200);
@@ -20,9 +13,6 @@ $('.avocado_nav ul > li').hover(
     function(){
         $(this).find('a > img').stop(true).animate({opacity:1},200);
     });
-//
-//02.sns_link:click
-//
 $('.sns_link').click(
     function(){
         var $inner = $('.sns_link_inner');
@@ -36,31 +26,7 @@ $('.sns_link').click(
                 }
             })
     });
-//
-//03.footer_link_box:hover
-//
-$('.fb').hover(
-    function(){
-        $(this).find('a').stop(true).animate({opacity:0.7},200);
-    },
-    function(){
-        $(this).find('a').stop(true).animate({opacity:1},200);
-    });
-$('.tw').hover(
-    function(){
-        $(this).find('a').stop(true).animate({opacity:0.7},200);
-    },
-    function(){
-        $(this).find('a').stop(true).animate({opacity:1},200);
-    });
-$('.yt').hover(
-    function(){
-        $(this).find('a').stop(true).animate({opacity:0.7},200);
-    },
-    function(){
-        $(this).find('a').stop(true).animate({opacity:1},200);
-    });
-$('.dfc').hover(
+$('.transparency').hover(
     function(){
         $(this).find('a').stop(true).animate({opacity:0.7},200);
     },
