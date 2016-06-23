9aec7c4a39a52539a43a731c070a99144e3d83f7
hiroki-ueno <tmge_0618@i.softbank.jp>
2016-06-23 12:31:21 +0900

    jsはまとめられる関数をまとめて、インデントを修正しました。 cssも、プロパティが重複しているものはまとめてあります。 htmlは一部class名の修正とomgタグ内のpx表示を削除しました。

--- curriculum-02/js/main.js
+++ curriculum-02/js/main.js
@@ -1,41 +1,34 @@
 $(function(){
-$('.dgfruit_nav ul > li').hover(
-    function(){
-        $(this).find('a > img').stop(true).animate({opacity:0},200);
-    },
-    function(){
-        $(this).find('a > img').stop(true).animate({opacity:1},200);
-    });
-$('.avocado_nav ul > li').hover(
-    function(){
-        $(this).find('a > img').stop(true).animate({opacity:0},200);
-    },
-    function(){
-        $(this).find('a > img').stop(true).animate({opacity:1},200);
-    });
-$('.sns_link').click(
-    function(){
-        var $inner = $('.sns_link_inner');
-        $inner.each(
-            function(){
-                $inner.toggleClass('show');
-                if($inner.hasClass('show')){
-                    $inner.stop(true).fadeIn(200);
-                }else{
-                    $inner.stop(true).fadeOut(200);
-                }
-            })
-    });
-$('.transparency').hover(
-    function(){
-        $(this).find('a').stop(true).animate({opacity:0.7},200);
-    },
-    function(){
-        $(this).find('a').stop(true).animate({opacity:1},200);
-    });
-$('.flush').on('mouseover',
-    function(){
-        $(this).animate({opacity:0.4},200);
-        $(this).animate({opacity:0},200);
-    });
+    $('.opa').hover(
+        function(){
+            $(this).find('a > img').stop(true).animate({opacity:0},200);
+        },
+        function(){
+            $(this).find('a > img').stop(true).animate({opacity:1},200);
+        });
+    $('.sns_link').click(
+        function(){
+            var $inner = $('.sns_link_inner');
+            $inner.each(
+                function(){
+                    $inner.toggleClass('show');
+                    if($inner.hasClass('show')){
+                        $inner.stop(true).fadeIn(200);
+                    }else{
+                        $inner.stop(true).fadeOut(200);
+                    }
+                })
+        });
+    $('.transparency').hover(
+        function(){
+            $(this).find('a').stop(true).animate({opacity:0.7},200);
+        },
+        function(){
+            $(this).find('a').stop(true).animate({opacity:1},200);
+        });
+    $('.flush').on('mouseover',
+        function(){
+            $(this).animate({opacity:0.4},200);
+            $(this).animate({opacity:0},200);
+        });
 });
